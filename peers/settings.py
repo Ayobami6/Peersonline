e27ad61657063046ce00761c1d590e55a3c75c15
Ayobami <ayobamidele006@gmail.com>
2023-06-25 20:02:20 +0100

    Add secondary test database

--- peers/settings.py
+++ peers/settings.py
@@ -129,6 +129,10 @@ DATABASES = {
             'USER': 'peers_admin',
         }
     },
+    'test': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # 'ENGINE': 'django.db.backends.sqlite3',
